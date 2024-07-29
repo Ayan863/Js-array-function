2e9b830c7f669d6944918440f4eeb636a25dc1b2
Ayan863 <GFGayanismayilzade2006@gmail.com>
2024-07-29 17:13:23 +0400

    first commit

--- assets/js/javas.js
+++ assets/js/javas.js
@@ -1,66 +1,18 @@
-// let arrayy = ["Ali", "Qabil", "Qabil", "Qabil", "Malik", "Kamil"];
-// task 1
-// arrow functiondan isdifade ederek ele bir function itifade edinki parametr olaraq array ve silinecek datani
-// gonderdiyinizde o function hemen datani silsin ve consolede silindiyi barede melumat cixsin eger data yoxdursa
-// oda ekranda cap olunsunki  data movcud deyil. meselen deleteFunc(arrayy,"Ali")
+// verilmis arraye uygunn olaraq arrow functiondan isdifade ederek dinamik olaraq
+// bir array ve bir simvol("herf") qebul eden funksiya yaradin
+// gonderilen herfle uygun olaraq o herfle baslayan adlari secib 
+// yeni arrayde ekrana cap elesin
+// qeyd: for of dan istifade ede bilersiz
 
-// let arrayy = ["Ali", "Qabil", "Qabil", "Qabil", "Malik", "Kamil"];
-// let count=0
-// const deleteFunc=(array,data)=>{
-//     let index=array.indexOf(data)
-//     if(index!=-1){
-//         while(index!=-1){
-//             array.splice(index,1)
-//             index=array.indexOf(data)
-//             count++
-//         }
-//         console.log(array)
-//         // console.log(data,"silindi");
-//         console.log(count,"defe qeyde alinmish",data,"adli data silindi");
-//     }
-//     else{
-//         let k=data ?? "boshdur"
-        
-//         if(k!="boshdur" && data!="" && data!=" "){
-//             console.log("qeyd olunan datani elave etmek olmadi");
-//         }
-//         else{
-//             console.log("yeniden sorgu gonderin");
-//         }
-//     }
-// }
-// // deleteFunc(arrayy,"Qabil")
-// deleteFunc(arrayy," ")
-
-
-// task 2
-//  bunun eynisini elave etmek ucunde isdifade edin yani arrow functiondan isdifade ederek array ve yeni deyeri
-// parametr olaraq vererek arraya elave olunsun ve  elave olunanda logda elave oldugu barede melumat cixsin
-// arraya data elave edende eyni elementin tekrar elave olundugunda logda bu datanin artiq var oldugunu  bildirsin
-// meselen addedFunc(arrayy,10)
-// let arraya = ["Ali", "Qabil", "Qabil", "Qabil", "Malik", "Kamil"];
-// const add=(array,data)=>{
-//     indexa=array.indexOf(data)
-//     if(indexa==-1){
-//         let k=data ?? "boshdur"
-        
-//         if(k!="boshdur" && data!="" && data!=" "){
-//             array.push(data)
-//             console.log(array);
-//             console.log(data+" elave olundu");
-//         }
-//         else{
-//             console.log("qeyd olunan datani elave etmek olmadi");
-//         }
-        
-//     }
-//     else{
-//         console.log(data+" adli data movcuddur");
-//     }
-// }
-// add(arraya,"")
-
-
-// QEYD : her iki functionda let in den isdifade etmeyiniz teleb olunur ! ve bos deyer ( bosluq veya null olaraq)
-// deyerin gonderilmesinide nezere alin yeni null ve bos deyer undefined gonderildiyinde logda gonderilen deyerin bos
-// olundugunu bildirilsin
+let arrayy = ["Ali", "Ali", "Qabil", "Qalib", "Behlul", "Ali", "Malik", "Kamil"];
+let array = []
+const search = (word) =>{
+    for(let index of arrayy){
+        if(word==index.charAt(0)){
+            array.push(index)
+        }
+    }
+    
+}
+search("K")
+console.log(array);
